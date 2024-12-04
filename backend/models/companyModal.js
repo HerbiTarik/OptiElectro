b9bc94f6e8df48a5367bab3ce82ddb0f90291dc8
Herbi <herbitarik2509@gmail.com>
2024-12-04 05:11:23 +0100

    Redesign of the CompanySearchScreen.js screen and addition of buttons for booking appointments

--- backend/models/companyModal.js
+++ backend/models/companyModal.js
@@ -3,7 +3,7 @@ const pool = require('../config/database');
 const companies = {
   getCompanies: async () => {
     const res = await pool.query(
-      `select entreprise.id, entreprise.denomination, entreprise.logo ,entreprise.stars, entreprise.activite_principale, ville.nom as city, entreprise.duree_intervention, entreprise.tarif_min, entreprise.tarif_max, entreprise.certification, entreprise.partenaire, TO_CHAR(entreprise.prochaine_disponibilite, 'DD Month YYYY') AS prochaine_disponibilite from entreprise inner join ville on entreprise.city = ville.id`,
+      `select entreprise.id, entreprise.denomination, entreprise.logo ,entreprise.stars, entreprise.activite_principale, ville.nom as city, entreprise.duree_intervention, entreprise.tarif_min, entreprise.tarif_max, entreprise.certification, entreprise.partenaire, TO_CHAR(entreprise.prochaine_disponibilite, 'DD TMMonth YYYY') AS prochaine_disponibilite from entreprise inner join ville on entreprise.city = ville.id`,
     );
     return res.rows;
   },
--- frontend/screens/CompanySearchScreen.js
+++ frontend/screens/CompanySearchScreen.js
@@ -1,4 +1,4 @@
-import {View, Image, Text, FlatList} from 'react-native';
+import {View, Image, Text, FlatList, Pressable} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import logoEnt from '../assets/logo_ent.jpg';
 import StarRating from 'react-native-star-rating-widget';
@@ -24,86 +24,92 @@ const CompanySearchScreen = () => {
   }, []);
 
   return (
-    <View className="flex-1 bg-fond">
+    <View className="flex-1 bg-fond mt-3">
       <FlatList
         data={data}
         keyExtractor={item => item.id}
         renderItem={({item}) => (
-          <View className="bg-white mx-3 mt-5 rounded-[10px] shadow-md shadow-[black]">
-            <View className="p-3">
-              <View className="flex-row">
-                <Image
-                  source={{uri: item.logo}}
-                  style={{
-                    resizeMode: 'cover',
-                    width: 70,
-                    height: 70,
-                    borderRadius: 50,
-                    borderWidth: 0.5,
-                    borderColor: 'black',
+          <View className="bg-white mx-3 my-5 rounded-[10px] shadow-md shadow-[black] p-3">
+            <View className="flex-row">
+              <Image
+                source={{uri: item.logo}}
+                style={{
+                  resizeMode: 'cover',
+                  width: 70,
+                  height: 70,
+                  borderRadius: 50,
+                  borderWidth: 0.5,
+                  borderColor: 'black',
+                }}
+              />
+              <View className="flex-auto justify-around pl-5">
+                <Text className="font-bold text-[22px] text-text2">
+                  {item.denomination}
+                </Text>
+
+                <StarRating
+                  rating={item.stars}
+                  onChange={setRating}
+                  maxStars={5}
+                  starSize={20}
+                  enableHalfStar
+                  color="#facc15"
+                  starStyle={{
+                    marginHorizontal: 2,
                   }}
                 />
-                <View className="flex-auto justify-around pl-5">
-                  <Text className="font-bold text-[22px] text-text2">
-                    {item.denomination}
-                  </Text>
-
-                  <StarRating
-                    rating={item.stars}
-                    onChange={setRating}
-                    maxStars={5}
-                    starSize={20}
-                    enableHalfStar
-                    color="#facc15"
-                    starStyle={{
-                      marginHorizontal: 2,
-                    }}
-                  />
-                </View>
               </View>
-              <View>
-                <Text className="font-bold text-text2 text-[15px] my-5">
-                  {item.activite_principale}
+            </View>
+            <View>
+              <Text className="font-bold text-text2 text-[15px] my-5">
+                {item.activite_principale}
+              </Text>
+              <View className="flex-row items-center mb-2 ">
+                <Ionicons name="pin-sharp" size={16} color="#0f172a" />
+                <Text className="text-text2  pl-2">
+                  Disponble à {item.city}
                 </Text>
-                <View className="flex-row items-center mb-2 ">
-                  <Ionicons name="pin-sharp" size={16} color="#0f172a" />
-                  <Text className="text-text2  pl-2">
-                    Disponble à {item.city}
-                  </Text>
-                </View>
-                <View className="flex-row items-center my-2 ">
-                  <Ionicons
-                    name="hourglass-outline"
-                    size={16}
-                    color="#0f172a"
-                  />
-                  <Text className=" text-text2 pl-2">
-                    <Text className="font-bold">Intervention moyenne </Text>:{' '}
-                    {item.duree_intervention} jours
-                  </Text>
-                </View>
-                <View className="flex-row items-center my-2 ">
-                  <Ionicons name="cash-outline" size={16} color="#0f172a" />
-                  <Text className=" text-text2 pl-2">
-                    <Text className="font-bold ">Tarifs</Text> :{' '}
-                    {item.tarif_min} - {item.tarif_max} €
-                  </Text>
-                </View>
-                <View className="flex-row items-center my-2 ">
-                  <MaterialIcons name="done" size={16} color="#0f172a" />
-                  <Text className="text-text2 pl-2">
-                    Certifié {item.certification} / Partenaire {item.partenaire}
-                  </Text>
-                </View>
-                <View className="bg-primary py-5 rounded-[10px] justify-center items-center mt-3">
-                  <Text className="text-txt font-bold mb-2">
-                    Prochaine disponibilté
-                  </Text>
-                  <Text className="text-btnColor ">
-                    {' '}
-                    {item.prochaine_disponibilite}
-                  </Text>
-                </View>
+              </View>
+              <View className="flex-row items-center my-2 ">
+                <Ionicons name="hourglass-outline" size={16} color="#0f172a" />
+                <Text className=" text-text2 pl-2">
+                  <Text className="font-bold">Intervention moyenne </Text>:{' '}
+                  {item.duree_intervention} jours
+                </Text>
+              </View>
+              <View className="flex-row items-center my-2 ">
+                <Ionicons name="cash-outline" size={16} color="#0f172a" />
+                <Text className=" text-text2 pl-2">
+                  <Text className="font-bold ">Tarifs</Text> : {item.tarif_min}{' '}
+                  - {item.tarif_max} €
+                </Text>
+              </View>
+              <View className="flex-row items-center my-2 ">
+                <MaterialIcons name="done" size={16} color="#0f172a" />
+                <Text className="text-text2 pl-2">
+                  Certifié {item.certification} / Partenaire {item.partenaire}
+                </Text>
+              </View>
+              <View className="bg-primary py-5 rounded-[10px] justify-center items-center mt-3">
+                <Text className="text-txt font-bold mb-2">
+                  Prochaine disponibilté
+                </Text>
+                <Text className="text-btnColor ">
+                  {' '}
+                  {item.prochaine_disponibilite}
+                </Text>
+              </View>
+            </View>
+            <View className="flex-row justify-between mt-4">
+              <View className="bg-btnColor p-3 rounded-lg">
+                <Pressable>
+                  <Text className="text-text2">En savoir plus</Text>
+                </Pressable>
+              </View>
+              <View className="bg-btnColor p-3 rounded-lg">
+                <Pressable>
+                  <Text className="text-text2">Prendre rendez-vous</Text>
+                </Pressable>
               </View>
             </View>
           </View>
